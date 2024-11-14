0084d5bd606fd4284dd172f14689290b4e37d52d
Geormary <engeormary@gmail.com>
2024-11-14 18:27:38 -0500

    created intial project login page focused on ui

---
+++ app/src/main/java/com/example/messengerapp_v1/ui/LoginScreen.kt
@@ -0,0 +1,103 @@
+package com.example.messengerapp_v1.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.messengerapp_v1.ui.theme.MessengerApp_V1Theme
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)
+            .background(color=Color(0xFF8BC34A)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+
+        ) {
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Email TextField
+        var email by remember { mutableStateOf("") }
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = TextFieldDefaults.colors(Color.White),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Email
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Password TextField
+        var password by remember { mutableStateOf("") }
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = TextFieldDefaults.colors(Color.White),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Password
+            )
+        )
+//
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Login Button
+        Button(
+            onClick = { /* Handle registration */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        ) {
+            Text(text = "Login")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+//        // Don't have an account Text
+        Text(
+            text = "Don't have an account?",
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .clickable {  }
+        )
+    }
+}
+// Add this Preview function below your composable function
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    MessengerApp_V1Theme() {
+        LoginScreen() // Call the RegisterScreen composable to preview
+    }
+}
---
+++ app/src/main/java/com/example/messengerapp_v1/ui/RegisterScreen.kt
@@ -0,0 +1,119 @@
+package com.example.messengerapp_v1.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.messengerapp_v1.ui.theme.MessengerApp_V1Theme
+
+@Composable
+fun RegisterScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)
+            .background(color=Color(0xFF8BC34A)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Username TextField
+        var username by remember { mutableStateOf("") }
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = TextFieldDefaults.colors(Color.White),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Email TextField
+        var email by remember { mutableStateOf("") }
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = TextFieldDefaults.colors(Color.White),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Email
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Password TextField
+        var password by remember { mutableStateOf("") }
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = TextFieldDefaults.colors(Color.White),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Password
+            )
+        )
+//
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Register Button
+        Button(
+            onClick = { /* Handle registration */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        ) {
+            Text(text = "Register")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Already have an account Text
+        Text(
+            text = "Already have an account?",
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
+// Add this Preview function below your composable function
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    MessengerApp_V1Theme() {
+        RegisterScreen() // Call the RegisterScreen composable to preview
+    }
+}
